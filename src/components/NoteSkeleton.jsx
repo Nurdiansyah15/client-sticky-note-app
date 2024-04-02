2118dcd4478d74716cca06a5a7d129b38fc4dfb2
Nurdiansyah <nurdinurdiansyah15@gmail.com>
2024-04-02 19:27:53 +0700

    animate pulse

--- src/components/NoteSkeleton.jsx
+++ src/components/NoteSkeleton.jsx
@@ -1,6 +1,6 @@
 function NoteSkeleton() {
   return (
-    <div className="w-full flex flex-col flex-1 mt-3 px-5">
+    <div className="w-full flex flex-col flex-1 mt-3 px-5 animate-pulse">
       <div className="h-4 w-1/3 bg-slate-500 rounded-full my-4 mb-12"></div>
       <div className="mb-3">
         <div className="h-3 w-full bg-slate-500 rounded-full mb-2"></div>
